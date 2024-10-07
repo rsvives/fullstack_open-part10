5c0c04b2a6434f6908ef867923c4f9dd2dfbce02
Rodri <r.serranvives@gmail.com>
2024-10-07 23:02:29 +0200

    app init

---
+++ rate-repository-app-sdk-51/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1}>React Native!⚛️🚀</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  h1:{
+    fontSize:32,
+    fontWeight:'800'
+  },
+  subtitle:{
+    fontSize:16
+  }
+});
