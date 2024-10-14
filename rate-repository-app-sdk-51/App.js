e3ecdcebc7033f69ec6266915bca2dec6575d2dc
Rodri <r.serranvives@gmail.com>
2024-10-14 18:20:07 +0530

    just playing

--- rate-repository-app-sdk-51/App.js
+++ rate-repository-app-sdk-51/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable, Alert } from 'react-native';
+// import {  } from 'react-native-web';
 
 export default function App() {
+
+const [text,setText]=useState('')
+
+const handleChange = (ev)=>{
+  setText(ev.target.value)
+}
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>React Native!⚛️🚀</Text>
+      <Text>text:{text}</Text>
+      <TextInput style={styles.input} id='text' value={text} inputMode='search'  keyboardType='default' onChangeText={setText} />
+      <Pressable style={styles.button} onPress={()=>Alert.alert('You pressed the text!')}><Text style={styles.buttonText}>Hola mundo</Text></Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,5 +34,20 @@ const styles = StyleSheet.create({
   },
   subtitle:{
     fontSize:16
+  },
+  input: {
+    height: 40,
+    width:'80%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button:{
+    padding:12,
+    backgroundColor:'#4af',
+    borderRadius:4
+  },
+  buttonText:{
+    color: '#fff'
   }
 });
