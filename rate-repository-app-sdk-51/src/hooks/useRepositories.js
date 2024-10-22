8340a34634b77d07a407b424c6a48308451ce81e
Rodri <r.serranvives@gmail.com>
2024-10-22 18:53:15 +0530

    ex 10.11 - GraphQL removing comments

--- rate-repository-app-sdk-51/src/hooks/useRepositories.js
+++ rate-repository-app-sdk-51/src/hooks/useRepositories.js
@@ -5,23 +5,6 @@ import { GET_REPOSITORIES } from "../graphql/queries"
 
 export const useRepositories = () => {
 
-    // const [repositories, setRepositories] = useState([])
-    // const [loading, setLoading] = useState(false)
-
-    // const fetchRepos = async () => {
-    //     setLoading(true)
-    //     const response = await fetch(`http://${LOCALHOST}:5001/api/repositories`)
-    //     const data = await response.json()
-
-    //     const mappedRepos = data.edges.map(e => e.node)
-    //     // console.log(data, mappedRepos)
-    //     setLoading(false)
-    //     console.log(mappedRepos)
-    //     setRepositories(mappedRepos)
-    // }
-
-    // useEffect(() => { fetchRepos() }, [])
-
     const { data, error, loading } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' })
     const repositories = data?.repositories.edges.map(e => e.node)
 
