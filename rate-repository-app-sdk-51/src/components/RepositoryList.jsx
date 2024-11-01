421ee723d625f3edee2e2c91ee154d6f4aac568e
Rodri <r.serranvives@gmail.com>
2024-11-01 13:59:07 +0530

    ex 10.24 - filtered repos without new requests to graphql server, just with filter

--- rate-repository-app-sdk-51/src/components/RepositoryList.jsx
+++ rate-repository-app-sdk-51/src/components/RepositoryList.jsx
@@ -3,11 +3,12 @@ import { useRepositories } from "../hooks/useRepositories"
 import { RepositoryListItem } from "./RepositoryListItem"
 import { Button, Menu, Divider, PaperProvider } from 'react-native-paper';
 import { Picker } from '@react-native-picker/picker';
-import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useReducer, useRef, useState } from "react";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Feather from '@expo/vector-icons/Feather';
 import { theme } from "../../theme";
 import { Text } from "./Text";
+import { TextInput } from "./TextInput";
 
 const styles = StyleSheet.create({
     separator: {
@@ -27,22 +28,50 @@ export const SORTING_OPTIONS = {
 }
 const SortingContext = createContext()
 
+const filteringReducer = (state, action) => {
+    switch (action.type) {
+        case 'search':
+            return {
+                ...state,
+                search: action.search,
+            }
+        case 'order:latest':
+            return {
+                ...state,
+                sorting: SORTING_OPTIONS.LATEST
+            }
+        case 'order:highest':
+            return {
+                ...state,
+                sorting: SORTING_OPTIONS.HIGHEST_RANKED
+            }
+        case 'order:lowest':
+            return {
+                ...state,
+                sorting: SORTING_OPTIONS.LOWEST_RANKED
+            }
+    }
+}
 
 const ItemSeparator = () => <View style={styles.separator} />
 
 
 export const RepositoryList = () => {
 
-    const [sorting, setSorting] = useState(SORTING_OPTIONS.LATEST)
-    const { repositories, loading } = useRepositories(sorting)
+    const [state, dispatch] = useReducer(filteringReducer, { search: '', sorting: SORTING_OPTIONS.LATEST })
+    const { repositories, loading } = useRepositories(state.sorting)
+    console.log(repositories)
+
+    const filteredRepositories = state.search !== '' ? repositories.filter(r => r.fullName.toLowerCase().includes(state.search.toLowerCase()) || r.description?.toLowerCase().includes(state.search.toLowerCase())) : repositories
 
     return (
         loading ?
             <Text style={{ textAlign: 'center', verticalAlign: 'middle', margin: 'auto' }}>Loading...</Text>
             :
-            <SortingContext.Provider value={{ sorting, setSorting }}>
+
+            <SortingContext.Provider value={{ state, dispatch }}>
                 <PaperProvider >
-                    <RepositoryContainer repositories={repositories} />
+                    <RepositoryContainer repositories={filteredRepositories} />
                 </PaperProvider >
             </SortingContext.Provider>
     )
@@ -62,7 +91,7 @@ export const RepositoryContainer = ({ repositories }) => {
                 renderItem={(item) => <RepositoryListItem item={item.item} />}
                 ItemSeparatorComponent={ItemSeparator}
                 keyExtractor={(item) => item.id}
-                ListHeaderComponent={<CustomPicker />}
+                ListHeaderComponent={<ListHeader />}
             />
         </>
 
@@ -72,17 +101,16 @@ export const RepositoryContainer = ({ repositories }) => {
 const CustomPicker = () => {
     const [visible, setVisible] = useState(false);
 
-    const { sorting, setSorting } = useContext(SortingContext)
+    const { state, dispatch } = useContext(SortingContext)
 
     const openMenu = () => setVisible(true);
     const closeMenu = () => setVisible(false);
     return (
 
-        <View style={{ alignItems: 'flex-end', marginVertical: theme.units.md, }}>
+        <View style={{ alignItems: 'flex-end', flexGrow: 1, flexBasis: 300, flexShrink: 1 }}>
             <View
                 style={{
                     minWidth: 'auto',
-                    maxWidth: 400,
                     width: '100%',
                     flexGrow: 1,
                 }}>
@@ -91,13 +119,13 @@ const CustomPicker = () => {
                     anchorPosition="bottom"
                     visible={visible}
                     onDismiss={closeMenu}
-                    anchor={<Select selectedOption={sorting} onPress={openMenu} />}>
+                    anchor={<Select selectedOption={state.sorting} onPress={openMenu} />}>
                     <View style={{ flexGrow: 1, width: '100%' }}>
-                        <Menu.Item style={{ flexGrow: 1 }} onPress={() => { setSorting(SORTING_OPTIONS.LATEST) }} title={SORTING_OPTIONS.LATEST} leadingIcon="clock-time-two-outline" />
+                        <Menu.Item style={{ flexGrow: 1 }} onPress={() => { dispatch({ type: 'order:latest' }) }} title={SORTING_OPTIONS.LATEST} leadingIcon="clock-time-two-outline" />
                         <Divider />
-                        <Menu.Item style={{ flexGrow: 1 }} onPress={() => { setSorting(SORTING_OPTIONS.HIGHEST_RANKED) }} title={SORTING_OPTIONS.HIGHEST_RANKED} leadingIcon="trending-up" />
+                        <Menu.Item style={{ flexGrow: 1 }} onPress={() => { dispatch({ type: 'order:highest' }) }} title={SORTING_OPTIONS.HIGHEST_RANKED} leadingIcon="trending-up" />
                         <Divider />
-                        <Menu.Item style={{ flexGrow: 1 }} onPress={() => { setSorting(SORTING_OPTIONS.LOWEST_RANKED) }} title={SORTING_OPTIONS.LOWEST_RANKED} leadingIcon="trending-down" />
+                        <Menu.Item style={{ flexGrow: 1 }} onPress={() => { dispatch({ type: 'order:lowest' }) }} title={SORTING_OPTIONS.LOWEST_RANKED} leadingIcon="trending-down" />
 
                     </View>
 
@@ -121,12 +149,7 @@ const Select = ({ onPress, selectedOption }) => {
             justifyContent: 'space-between',
             alignItems: 'center',
             flexGrow: 1,
-            // flexShrink: 1,
             flexBasis: 1,
-            // alignSelf: 'flex-end',
-            // width: '100%',
-            // maxWidth: 300
-
         },
     })
     return (
@@ -143,6 +166,35 @@ const Select = ({ onPress, selectedOption }) => {
 
         </View>
 
+    )
+}
+
+const ListHeader = () => {
+    return (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap', gap: theme.units.md, marginBottom: theme.units.xl, marginTop: theme.units.md }}>
+            <CustomSearchBar />
+            <CustomPicker />
+        </View>
+    )
+}
+
+const CustomSearchBar = () => {
+
+    const { state, dispatch } = useContext(SortingContext)
+
+    const handleChange = (event) => {
+        console.log(event)
+        dispatch({ type: 'search', search: event })
+    }
+    return (
+        <View style={{ flexGrow: 1, flexBasis: 300, flexShrink: 1 }}>
+            <TextInput
+                value={state.search}
+                placeholder={'Search a repository...'}
+                onChangeText={handleChange}
+            />
+
+        </View>
     )
 }
 
